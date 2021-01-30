49fea92cd8a4130d9c551a9d47c8178143ea2466
Andrew Rivett <veggiefrog@gmail.com>
2021-01-29 20:59:44 -0500

    Updated scripts

--- scripts/cron.php
+++ scripts/cron.php
@@ -19,7 +19,6 @@ if( !file_exists($ciniki_root . '/ciniki-api.ini') ) {
 // loadMethod is required by all function to ensure the functions are dynamically loaded
 require_once($ciniki_root . '/ciniki-mods/core/private/loadMethod.php');
 require_once($ciniki_root . '/ciniki-mods/core/private/init.php');
-require_once($ciniki_root . '/ciniki-mods/core/private/checkModuleFlags.php');
 
 $rc = ciniki_core_init($ciniki_root, 'rest');
 if( $rc['stat'] != 'ok' ) {
